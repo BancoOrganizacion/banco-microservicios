aa52c0cc6da986e3356fd41ce524f52268c02c04
CATEVA26 <119013401+CATEVA26@users.noreply.github.com>
2025-05-22 17:02:30 -0500

    conexion bd patterns

--- patterns-service/src/app.module.ts
+++ patterns-service/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PatternController } from './pattern/pattern.controller';
-import { PatternService } from './pattern/pattern.service';
 import { PatternModule } from './pattern/pattern.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PatternModule],
-  controllers: [AppController, PatternController],
-  providers: [AppService, PatternService],
+  imports: [MongooseModule.forRoot('mongodb://admin:Banco123*@localhost:27018/bancodb?authSource=admin'),PatternModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- patterns-service/src/pattern/pattern.module.ts
+++ patterns-service/src/pattern/pattern.module.ts
@@ -2,14 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PatternService } from './pattern.service';
 import { PatternController } from './pattern.controller';
-
-// Importa los modelos y esquemas correctamente
-import {
-  PatronAutenticacion,
-  PatronAutenticacionSchema,
-  DedoPatron,
-  DedoPatronSchema,
-} from 'shared-models';
+import { PatronAutenticacion, PatronAutenticacionSchema } from 'shared-models';
+import { DedoPatron, DedoPatronSchema } from 'shared-models';
 
 @Module({
   imports: [
@@ -18,7 +12,8 @@ import {
       { name: DedoPatron.name, schema: DedoPatronSchema },
     ]),
   ],
-  controllers: [PatternController],
   providers: [PatternService],
+  controllers: [PatternController],
+  exports: [PatternService],
 })
 export class PatternModule {}
