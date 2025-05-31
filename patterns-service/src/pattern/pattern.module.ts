239ff295cd04190bb85d296be9f1dfa6e1a2810f
CATEVA26 <119013401+CATEVA26@users.noreply.github.com>
2025-05-30 21:31:15 -0500

    fix pattern

--- patterns-service/src/pattern/pattern.module.ts
+++ patterns-service/src/pattern/pattern.module.ts
@@ -2,17 +2,25 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PatternService } from './pattern.service';
 import { PatternController } from './pattern.controller';
-import { PatronAutenticacion, PatronAutenticacionSchema } from 'shared-models';
+import { CuentaApp, CuentaAppSchema, PatronAutenticacion, PatronAutenticacionSchema } from 'shared-models';
 import { DedoPatron, DedoPatronSchema } from 'shared-models';
+import { Schema as MongooseSchema } from 'mongoose';
+import { JwtDataGuard } from 'src/guards/jwt-data.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: PatronAutenticacion.name, schema: PatronAutenticacionSchema },
       { name: DedoPatron.name, schema: DedoPatronSchema },
+      {name:CuentaApp.name, schema:CuentaAppSchema},
     ]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
-  providers: [PatternService],
+  providers: [PatternService,JwtDataGuard],
   controllers: [PatternController],
   exports: [PatternService],
 })
